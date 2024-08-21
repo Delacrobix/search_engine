c3bdc54b2f2c3f9c37c0ecf5e8394d87f811316a
Jeffrey <jeffrey.rengifom@gmail.com>
2024-08-21 18:24:36 -0500

    results component improvement

--- frontend-se/src/componentes/results.tsx
+++ frontend-se/src/componentes/results.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Results as ElasticResults } from "@elastic/react-search-ui";
 import { EuiCard } from "@elastic/eui";
+import { useNavigate } from "react-router-dom";
 
 export default function Results() {
   return (
     <ElasticResults
       className='grid grid-cols-2 gap-3'
+      titleField='name'
       resultView={ResultsView}
     />
   );
@@ -13,14 +14,16 @@ export default function Results() {
 
 interface ResultsViewProps {
   result: any;
-  onClickLink: () => void;
 }
 
-function ResultsView({ result, onClickLink }: Readonly<ResultsViewProps>) {
+function ResultsView({ result }: Readonly<ResultsViewProps>) {
   console.log(result);
+  const navigate = useNavigate();
 
   function handlerClick() {
-    console.log("Clicked");
+    const albumId = result.id.raw;
+
+    navigate(`/album/${albumId}`);
   }
 
   return (
@@ -32,21 +35,26 @@ function ResultsView({ result, onClickLink }: Readonly<ResultsViewProps>) {
       icon={
         <img
           className=' max-w-32 max-h-32'
-          src={result.images.raw[1].url}
+          src={result.images.raw[2].url}
           alt={`${result.name.raw}-album-cover`}
         />
       }
       title={<h3 className=' text-cyan-900 py-1'>{result.name.raw}</h3>}
       description={
-        <span className='block '>
-          <span className='block text-gray-700 mb-2'>
-            {"Artists: " +
-              result.artists.raw.map((artist: any) => artist.name).join(", ")}
+        <span className='flex gap-4'>
+          <span>
+            <span className='block text-gray-700 mb-2'>
+              {"Artists: " +
+                result.artists.raw.map((artist: any) => artist.name).join(", ")}
+            </span>
+            <span className='block text-gray-700 mb-2'>
+              {"Release date: " + result.release_date.raw}
+            </span>
+            <span className='block text-gray-700 mb-2'>
+              {"Tracks: " + result.total_tracks.raw}
+            </span>
+            <span className='block text-gray-700'>{result.album_type.raw}</span>
           </span>
-          <span className='block text-gray-700 mb-2'>
-            {"Release date: " + result.release_date.raw}
-          </span>
-          <span className='block text-gray-700'>{result.album_type.raw}</span>
         </span>
       }
     />
