0d9a80e65a55355724b52b87d60817bd4792da9b
Jeffrey <jeffrey.rengifom@gmail.com>
2024-08-21 18:27:11 -0500

    rotuting added

--- frontend-se/src/App.tsx
+++ frontend-se/src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Index from "./views";
+import Album from "./views/album";
 
 export default function App() {
-  return <Index />;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/album/:id' element={<Album />} />
+      </Routes>
+    </Router>
+  );
 }
