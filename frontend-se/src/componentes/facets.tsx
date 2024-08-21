5b8388825a2373eb34cdd1e738a0fc2dfb492aef
Jeffrey <jeffrey.rengifom@gmail.com>
2024-08-21 18:23:39 -0500

    changes in facets component

--- frontend-se/src/componentes/facets.tsx
+++ frontend-se/src/componentes/facets.tsx
@@ -7,25 +7,25 @@ import { getUuidString } from "../utils/functions";
 interface FacetsProps {
   field: string;
   label: string;
+  show?: number;
 }
 
-export default function Facets({ field, label }: Readonly<FacetsProps>) {
-  return <Facet field={field} label={label} view={FacetsView} />;
+export default function Facets({ field, label, show }: Readonly<FacetsProps>) {
+  return <Facet field={field} show={show} label={label} view={FacetsView} />;
 }
 
 function FacetsView({
-  // values,
-  // onChange,
-  // onSearch,
-  // showMore,
-  show,
+  values,
+  onChange,
+  onSearch,
+  showMore,
   label,
   onMoreClick,
   onRemove,
   onSelect,
   options,
 }: Readonly<FacetViewProps>) {
-  console.log("show", show);
+  console.log("labels: ", label);
 
   function handleSelection(value: string) {
     const isSelected = options.find(
@@ -43,23 +43,26 @@ function FacetsView({
       <h3 className='text-lg text-nowrap font-semibold pb-1'>{label}</h3>
       <ul className=' text-nowrap'>
         {options.map((option) => (
-          <li key={getUuidString()} className=''>
+          <li key={getUuidString()}>
             <input
               className='mr-2 cursor-pointer'
               type='checkbox'
               id={getUuidString()}
               checked={option.selected}
-              onChange={() => handleSelection(option.value)}
+              onChange={() => handleSelection(option.value as string)}
             />
-            <label htmlFor={option.value} className=''>
+            <label htmlFor={option.value as string}>
               {option.value} ({option.count})
             </label>
           </li>
         ))}
       </ul>
-      {options.length > show && (
-        <button onClick={onMoreClick} className=''>
-          More
+      {showMore && (
+        <button
+          className='text-blue-500 cursor-pointer'
+          onClick={onMoreClick}
+          disabled={!showMore}>
+          Show more
         </button>
       )}
     </div>
