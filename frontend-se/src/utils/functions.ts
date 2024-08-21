9f63a2bef2964c93679f708ae258d82158c2e2f8
Jeffrey <jeffrey.rengifom@gmail.com>
2024-08-21 18:27:53 -0500

    formatDate function added to utils

--- frontend-se/src/utils/functions.ts
+++ frontend-se/src/utils/functions.ts
@@ -3,3 +3,11 @@ import { v4 as uuidv4 } from "uuid";
 export function getUuidString(): string {
   return uuidv4();
 }
+
+export function formatDate(date: string): string {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
